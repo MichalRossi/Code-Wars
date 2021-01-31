9dad4e0505d0caa7c982493681754a77c36627d2
Michał Rossi <michal1.rosiak@gmail.com>
2021-01-31 20:17:04 +0100

    Another solutions

---
+++ src/test/java/BraceCheckerTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class BraceCheckerTest {
+
+    private BraceChecker checker = new BraceChecker();
+
+    @Test
+    public void testValid() {
+        assertEquals(true, checker.isValid("()"));
+    }
+
+    @Test
+    public void testInvalid() {
+        assertEquals(false, checker.isValid("[(])"));
+    }
+
+    @Test
+    public void testValid2() {
+        assertEquals(true, checker.isValid("(){}[]"));
+    }
+
+
+}
